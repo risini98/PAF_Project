387a3f20338f4a1308e0fb1a86356a199803124b
visnaOshani <jayasinghe548@gmail.com>
2020-04-18 11:26:46 +0530

    implemented appointment class

--- HealthCareSystem/src/com/Appointment.java
+++ HealthCareSystem/src/com/Appointment.java
@@ -1,5 +1,46 @@
 package com;
+import javax.xml.bind.annotation.XmlRootElement;
 
+import java.sql.Date;
+
+@XmlRootElement
 public class Appointment {
 
+	private int id;
+	private String date;
+	private String time;
+	private String description;
+	private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 }
